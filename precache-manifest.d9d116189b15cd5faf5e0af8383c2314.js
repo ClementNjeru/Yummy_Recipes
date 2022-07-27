1c4c4a6678eecd36a3acd05e71279671366927ce
ClementNjeru <njeruclement36@gmail.com>
2022-07-27 15:31:04 +0300

    Updates

---
+++ precache-manifest.d9d116189b15cd5faf5e0af8383c2314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84b4133865fd9242d3de",
+    "url": "/Yummy_Recipes/static/css/main.a542b6b6.chunk.css"
+  },
+  {
+    "revision": "84b4133865fd9242d3de",
+    "url": "/Yummy_Recipes/static/js/main.84b41338.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/Yummy_Recipes/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "d2a60a3e8d8297467d00",
+    "url": "/Yummy_Recipes/static/js/2.d2a60a3e.chunk.js"
+  },
+  {
+    "revision": "a9b807c8047b5aba73bd",
+    "url": "/Yummy_Recipes/static/js/runtime~main.a9b807c8.js"
+  },
+  {
+    "revision": "46a4dede9e12f2847b93df3b8bf40402",
+    "url": "/Yummy_Recipes/index.html"
+  }
+];
