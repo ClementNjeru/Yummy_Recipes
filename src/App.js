fbaf6d1a124d9b5add1971c5413ff146cfa5a8c3
ClementNjeru <njeruclement36@gmail.com>
2022-07-28 14:30:20 +0300

    modifies the App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Recipe from './Components/Recipe';
 import './Components/style.css';
+import { BrowserRouter as Routes, Route } from "react-router-dom";
+// import RecipeItem from './Components/RecipeItem';
 
 
 function App() {
   return (
-    
-    <Recipe/>
+    <>
+        <Router>
+
+          <Routes>
+              <Route exact path="/" element={<RecipeItem/>}>
+              </Route>            
+          </Routes>
+         </Router>
+    </>
     
   )
 }
