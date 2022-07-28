574abfcdcbad98ce049abd5839c2dec7ebb81ce5
ClementNjeru <njeruclement36@gmail.com>
2022-07-28 14:28:39 +0300

    modifies the MealInstructions.js file

---
+++ src/Components/MealInstructions.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const MealInstructions=()=>{
+    return(
+        <>
+           <h3>Cooking Instructions</h3>
+        
+        </>
+    )
+}
+export default MealInstructions;
